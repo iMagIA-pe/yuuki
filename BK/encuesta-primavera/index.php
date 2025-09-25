831385d08636cc31cf1d633613c6789835028d3e
belleavm <bellanaid@gmail.com>
2025-09-25 12:24:50 -0500

    cambio de hora

---
+++ BK/encuesta-primavera/index.php
@@ -0,0 +1,5 @@
+<?php
+// Redireccionar a otra pagina
+header("Location: https://tally.so/r/mDLN5R?utm_source=encuesta&utm_medium=link&utm_campaign=marcha_blanca&utm_local=primavera&utm_content=compartido");
+exit();
+?>
